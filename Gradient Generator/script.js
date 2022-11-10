e8afd48e5e5b66d507a767a40099d0457564d813
Chetan <106894472+ChetanIt@users.noreply.github.com>
2022-11-10 20:58:13 +0530

    P1c
    Completed basic Functionality

--- Gradient Generator/script.js
+++ Gradient Generator/script.js
@@ -1,14 +1,22 @@
 var r = document.querySelector(':root');
 var dis = document.getElementById("colorDis");
-color1 = document.getElementById('col1').value;
-color2 = document.getElementById('col2').value;
-color3 = document.getElementById('col3').value;
+
 
 
 function grad(){
+    color1 = document.getElementById('col1').value;
+    color2 = document.getElementById('col2').value;
+    color3 = document.getElementById('col3').value;
     angle_input = document.getElementById('angle').value;
-    dis.style.backgroundImage =  "linear-gradient(" + angle + "deg"+ "," + color1 + "," + color2 + "," + color3 + ")";
+    dis.style.background =  "linear-gradient("
+     + angle_input
+     + "deg, " 
+     + color1 
+     + "," 
+     + color2 
+     + "," 
+     + color3 
+     + ")";
     dis.style.borderColor = color1;
     console.log(dis.style.background);
-
 }
