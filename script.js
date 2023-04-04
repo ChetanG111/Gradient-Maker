62acf8ac44a877bf11e265b788e9755ea44f2dd2
ChetanG_111 <84891627+ChetanG111@users.noreply.github.com>
2023-04-04 20:04:08 +0530

    P21

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-var r = document.querySelector(':root');
-var dis = document.getElementById("colorDis");
-
-
-
-function grad(){
-    color1 = document.getElementById('col1').value;
-    color2 = document.getElementById('col2').value;
-    color3 = document.getElementById('col3').value;
-    angle_input = document.getElementById('angle').value;
-    dis.style.background =  "linear-gradient("
-     + angle_input
-     + "deg, " 
-     + color1 
-     + "," 
-     + color2 
-     + "," 
-     + color3 
-     + ")";
-    dis.style.borderColor = color1;
-    console.log(dis.style.background);
-}
-
 //copy color hex code to clipboard
-function copy_to_clip(color_code, display){
+function copy_to_clip(code, display){
     //copies the value of the color to the clipboard
-    navigator.clipboard.writeText(color_code);
+    navigator.clipboard.writeText(code);
     display.value = 'Copied';
     //Waits for 250 milliseconds and then executes the line written in curly braces
-    setTimeout(() =>{display.value = color_code}, 250);
+    setTimeout(() =>{display.value = code}, 250);
 }
 
+// Get all the h3 elements
+const h3Elements = document.querySelectorAll('.card h3');
+
+// Loop through the h3 elements and add a click event listener to each one
+h3Elements.forEach((h3) => {
+  h3.addEventListener('click', () => {
+    // Change the text of the clicked h3 element to "Copied"
+    h3.textContent = 'Copied';
+    
+    // Get the corresponding cardDis element and its background-image style value
+    const cardDis = h3.parentNode.querySelector('.cardDis');
+    const bgImage = window.getComputedStyle(cardDis).getPropertyValue('background-image');
+    
+    // Copy the style value to the clipboard
+    navigator.clipboard.writeText(bgImage);
+
+    setTimeout(() =>{h3.innerHTML = 'Copy CSS'}, 250);
+
+  });
+});
